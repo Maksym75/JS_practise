b138c70f8a7ac07b8ca08cabe9f08989e315ceee
MaksymKolii <maksymkolii@gmail.com>
2023-09-22 00:32:24 +0200

    commited after 35 lesson

--- script.js
+++ script.js
@@ -11,7 +11,20 @@
 
 "use strict";
 
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+let numberOfFilms;
+
+function start() {
+  while (
+    numberOfFilms == "" ||
+    numberOfFilms == null ||
+    isNaN(numberOfFilms) ||
+    numberOfFilms <= 0
+  ) {
+    // alert("Wrong answer! Try again");
+    numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "").trim();
+  }
+}
+start();
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -21,18 +34,28 @@ const personalMovieDB = {
   privat: false,
 };
 
-for (let i = 0; i < 2; i++) {
-  const a = prompt("Один из последних просмотренных фильмов?", ""),
-    b = prompt("На сколько оцените его?", "");
-
-  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-    personalMovieDB.movies[a] = b;
-    console.log("DONE");
-  } else {
-    console.log("ERROR");
-    i--;
+function showMyDB(obj) {
+  if (!obj) {
+    console.log(obj);
   }
 }
+
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?", "").trim(),
+      b = prompt("На сколько оцените его?", "").trim();
+
+    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+      console.log("DONE");
+    } else {
+      console.log("ERROR");
+      i--;
+    }
+  }
+}
+
+rememberMyFilms();
 
 // let count = 0;
 // while (count < 2) {
@@ -47,46 +70,114 @@ for (let i = 0; i < 2; i++) {
 //     console.log("ERROR");
 //   }
 // }
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10) {
+    console.log('"Просмотрено довольно мало фильмов"');
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+  } else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+  } else console.log("Произошла ошибка");
+}
+detectPersonalLevel();
 
-if (personalMovieDB.count < 10) {
-  console.log('"Просмотрено довольно мало фильмов"');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-  console.log("Вы классический зритель");
-} else if (personalMovieDB.count >= 30) {
-  console.log("Вы киноман");
-} else console.log("Произошла ошибка");
+function writeYourGenres(obj) {
+  for (let i = 1; i <= 3; i++) {
+    const a = prompt(`Ваш любимый жанр под номером ${i}`, "").trim();
 
+    if (a !== "" && a !== null && typeof a === "string") {
+      obj.genres[i - 1] = a;
+    } else {
+      console.log("Wrong GENRE Type");
+      i--;
+    }
+  }
+}
+
+// showMyDB(personalMovieDB1);
+
+writeYourGenres(personalMovieDB);
+showMyDB(personalMovieDB);
 console.log("personalMovieDB", personalMovieDB);
 
-function sayHello(name) {
-  return "Привет " + `${name}`;
-}
+function BinarySearch(t, A) {
+  // t - искомый элемент,
+  // A - упорядоченный массив, в котором ищем.
+  let i = 0,
+    j = A.length - 1,
+    k;
 
-sayHello("Masha");
-console.log(sayHello("Masha"));
-
-function returnNeighboringNumbers(numb) {
-  // let arr = [];
-  // arr[0] = numb - 1;
-  // arr[1] = numb;
-  // arr[2] = numb + 1;
-  return [numb - 1, numb, numb + 1];
-}
-
-function getMathResult(base, increase) {
-  if (increase <= 0 || typeof increase !== "number") {
-    // console.log(base);
-    return base;
+  while (i <= j) {
+    k = Math.floor((i + j) / 2);
+    if (t === A[k]) return k;
+    else if (t < A[k]) j = k - 1;
+    else i = k + 1;
   }
-  let str = "";
-
-  for (let i = 1; i <= increase; i++) {
-    if (i === increase) {
-      str += base * i;
-    } else str += `${base * i}---`;
-  }
-  return str;
+  // На выходе индекс искомого элемента.
+  return -1; // Если искомого элемента нет в массиве, то -1.
 }
 
-console.log(getMathResult(14, 22));
-console.dir(Number);
+const ar = [-190, -22, 2, 9, 16, 87, 945, 1000];
+
+// console.log(BinarySearch(87, ar));
+
+const personalMovieDB1 = {
+  count: 99,
+  movies: { СтарТрек: 7.9, "Need for Speed": 9.99 },
+  actors: {},
+  genres: ["Action", "Criminal"],
+  maketest: () => console.log("Test"),
+  privat: false,
+};
+//??   Подсчёт количества элементов (1 уровень вложенности) в Объекте  =====================================================
+// console.log(Object.keys(personalMovieDB1).length);
+
+//??   Подсчёт количества элементов (2 уровень вложенности) в Объекте  =====================================================
+// console.log(Object.keys(personalMovieDB1.movies).length);
+
+//?? 1 Поверхностное  Копирование Объекта  =====================================================
+function copyObject(mainObj) {
+  let copiedObj = {};
+  let key;
+  for (key in mainObj) {
+    copiedObj[key] = mainObj[key];
+  }
+  return copiedObj;
+}
+// console.log(copyObject(personalMovieDB1));
+//?? 2 Поверхностное  Копирование Объекта  =====================================================
+const clone = Object.assign({}, personalMovieDB1);
+clone.count = 7;
+console.log(clone);
+console.log(personalMovieDB1);
+// personalMovieDB1.maketest();
+
+//?? 3 Поверхностное  Копирование Массива  =====================================================
+const oldArray = ["b", "a", "g"];
+const copiedArray = oldArray.slice();
+copiedArray[0] = "a";
+console.log(oldArray);
+console.log(copiedArray);
+
+//?? 4 Поверхностное  Копирование  =====================================================
+const video = ["youtube", "vimeo", "rutube"],
+  bloggs = ["wordpress", "livejournal", "blogger"],
+  // internet = [];
+  internet = [...bloggs, ...video];
+const arrr = ["Start", ...internet, "Finish"];
+console.log(internet);
+console.log(arrr);
+
+function log(a, b, c) {
+  console.log(a);
+  console.log(b);
+  console.log(c);
+}
+const numbers = [5, 88, 32, -1];
+log(...numbers);
+
+const copyNumbers = [...numbers];
+console.log("copyNumbers", copyNumbers);
+
+const cloneObject = { ...personalMovieDB1 };
+console.log("cloneObject", cloneObject);
--- script2.js
+++ script2.js
@@ -1,46 +1,4 @@
-/* Задание на урок:
-
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
-
-Проверить, чтобы все работало без ошибок в консоли */
-
 "use strict";
-
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
-};
-
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-  b = prompt("На сколько оцените его?", ""),
-  c = prompt("Один из последних просмотренных фильмов?", ""),
-  d = prompt("На сколько оцените его?", "");
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-console.log("personalMovieDB", personalMovieDB);
 
 console.log(NaN || 2 || undefined);
 
@@ -213,3 +171,233 @@ function thirdTask() {
   }
   return result;
 }
+
+function sayHello(name) {
+  return "Привет " + `${name}`;
+}
+
+sayHello("Masha");
+console.log(sayHello("Masha"));
+
+function returnNeighboringNumbers(numb) {
+  return [numb - 1, numb, numb + 1];
+}
+
+function getMathResult(base, increase) {
+  if (increase <= 0 || typeof increase !== "number") {
+    // console.log(base);
+    return base;
+  }
+  let str = "";
+
+  for (let i = 1; i <= increase; i++) {
+    if (i === increase) {
+      str += base * i;
+    } else str += `${base * i}---`;
+  }
+  return str;
+}
+
+console.log(getMathResult(14, 22));
+console.dir(Number);
+
+//1) Создайте функцию, которая будет вычислять объем и площадь полной поверхности куба
+function calculateVolumeAndArea(data) {
+  if (data < 0 || typeof data !== "number" || !Number.isInteger(data)) {
+    return "При вычислении произошла ошибка";
+  } else {
+    let v = data * data * data,
+      s = 6 * (data * data);
+    return `Объем куба: ${v}, площадь всей поверхности: ${s}`;
+  }
+}
+console.log(calculateVolumeAndArea(15));
+
+function getCoupeNumber(seatNumber) {
+  if (
+    typeof seatNumber !== "number" ||
+    seatNumber < 0 ||
+    !Number.isInteger(seatNumber)
+  ) {
+    return "Ошибка. Проверьте правильность введенного номера места";
+  }
+
+  if (seatNumber === 0 || seatNumber > 36) {
+    return "Таких мест в вагоне не существует";
+  }
+
+  return Math.ceil(seatNumber / 4);
+  // тут очень много вариантов решения, но выбрал один из элегантных :)
+}
+
+console.log(getCoupeNumber(32));
+
+function getTimeFromMinutes(time) {
+  if (typeof time !== "number" || time < 0 || !Number.isInteger(time)) {
+    return "Ошибка. Проверьте правильность введенного времени";
+  }
+
+  let hours = Math.floor(time / 60);
+  let minutes = time % 60;
+  let hoursStr = "";
+
+  switch (hours) {
+    case 0:
+      hoursStr = "часов";
+      break;
+    case 1:
+    case 21:
+      hoursStr = "час";
+      break;
+    case 2:
+    case 3:
+    case 4:
+    case 22:
+    case 23:
+    case 24:
+      hoursStr = "часа";
+      break;
+    default:
+      hoursStr = "часов";
+  }
+
+  // return `Это ${hours} ${hoursStr} и ${minutes} минут`;
+  if (hours > 24) {
+    let days = Math.floor(hours / 24);
+    hours = hours % 24;
+    switch (hours) {
+      case 0:
+        hoursStr = "часов";
+        break;
+      case 1:
+      case 21:
+        hoursStr = "час";
+        break;
+      case 2:
+      case 3:
+      case 4:
+      case 22:
+      case 23:
+      case 24:
+        hoursStr = "часа";
+        break;
+      default:
+        hoursStr = "часов";
+    }
+    return `"Это ${days} days  ${hours} ${hoursStr} и ${minutes} минут`;
+  }
+  return `"Это ${hours} ${hoursStr} и ${minutes} минут`;
+}
+
+console.log(getTimeFromMinutes(7126));
+
+//*   Fibonacci
+function fib(num) {
+  if (typeof num !== "number" || num <= 0 || !Number.isInteger(num)) {
+    return "";
+  }
+
+  let result = "";
+  let first = 0;
+  let second = 1;
+
+  for (let i = 0; i < num; i++) {
+    if (i + 1 === num) {
+      result += `${first}`;
+      // Без пробела в конце
+    } else {
+      result += `${first} `;
+    }
+
+    let third = first + second;
+    first = second;
+    second = third;
+  }
+
+  return result;
+}
+
+console.log(fib(6));
+
+const personalPlanPeter = {
+  name: "Peter",
+  age: "29",
+  skills: {
+    languages: ["ru", "eng"],
+    programmingLangs: {
+      js: "20%",
+      php: "10%",
+    },
+    exp: "1 month",
+  },
+  showAgeAndLangs: function (data) {
+    const { age } = data;
+    const { languages } = data.skills;
+    let str = `Мне ${age} и я владею языками: `;
+    // let langStr = "";
+    // languages.forEach((a) => (langStr += `${a.toUpperCase()} `));
+    // return str + langStr;
+    languages.forEach((a) => (str += `${a.toUpperCase()} `));
+    return str;
+  },
+};
+
+console.log(personalPlanPeter.showAgeAndLangs(personalPlanPeter));
+
+function showExperience(plan) {
+  const { exp } = plan.skills;
+  // console.log("typeof exp", typeof exp);
+  return exp;
+}
+// console.log(showExperience(personalPlanPeter));
+
+// 2) Напишите функцию showProgrammingLangs, которая будет принимать в себя объект со всеми данными и возвращать строку в нужном виде.
+// Пример:
+
+// showProgrammingLangs(personalPlanPeter)  =>
+
+// "Язык js изучен на 20% Язык php изучен на 10%"
+
+// Причем функция должна работать вне зависимости от количества языков. Если ни один не указан, то возвращается пустая строка.
+
+// P.S. Для переноса строки используется \n в конце строки.
+
+function showProgrammingLangs(plan) {
+  const { programmingLangs } = plan.skills;
+  let result = "";
+
+  for (let key in programmingLangs) {
+    result += `Язык ${key} изучен на ${programmingLangs[key]}\n`;
+  }
+  return result;
+}
+
+// console.log(showProgrammingLangs(personalPlanPeter));
+
+// 3) Создайте метод showAgeAndLangs внутри объекта personalPlanPeter. При его вызове метод будет принимать в себя объект и возвращать строку в нужном виде.
+
+// Пример:
+
+// personalPlanPeter.showAgeAndLangs(personalPlanPeter)
+// => 'Мне 29 и я владею языками: RU ENG'
+
+// Заметьте, что возраст и языки подставляются автоматически из объекта, а языки всегда в верхнем регистре (большими буквами). Если данные в объекте поменяются, то и сообщение тоже изменится.
+
+// * Напишите функцию showFamily, которая будет принимать в себя массив строк и возвращать сообщение в нужном формате.
+
+// showFamily(family)  => 'Семья состоит из: Peter Ann Alex Linda'
+
+// Имена подставляются автоматически из массива. Если массив пустой, то выводится сообщение 'Семья пуста'
+const family = ["Peter", "Ann", "Alex", "Linda"];
+
+function showFamily(arr) {
+  let str = "Семья состоит из: ";
+  if (arr.length === 0) {
+    return "Семья пуста";
+  }
+  arr.forEach((a) => (str += `${a} `));
+  return str;
+}
+console.log(showFamily(family));
+const asss = [];
+console.log(showFamily(asss));
